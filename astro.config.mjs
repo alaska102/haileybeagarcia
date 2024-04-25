fb7574f8ef5548b9a14a1e2042caf3d63243f0ec
“Hailey <haileyg102@berkeley.edu>
2024-04-25 13:20:33 -0700

    hosting

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://haileybeagarcia.com',
   integrations: [mdx(), sitemap(), icon()]
 });
