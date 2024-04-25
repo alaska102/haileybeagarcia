2d5ca268fb88a73e9b55e530f953259da380957d
“Hailey <haileyg102@berkeley.edu>
2024-04-25 13:07:04 -0700

    website

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Hailey Garcia';
+export const SITE_DESCRIPTION = 'come learn a bit more about me ;)!';
